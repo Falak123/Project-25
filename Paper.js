240c6e6e870e4259ffd0f083fc265a321c13b54c
Falak123 <82708235+Falak123@users.noreply.github.com>
2021-07-24 17:43:53 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,6 +1,6 @@
 class Paper
 {
-	constructor(x,y)
+	constructor(x,y,r)
 	{
 		var options={
 			isStatic:false,
@@ -10,6 +10,7 @@ class Paper
 			}
 		this.x=x;
 		this.y=y;
+		this.r = r;
 		this.image = loadImage("paper.png");
 		this.body=Bodies.circle(this.x,this.y,(this.r-20)/2 , options);
  		World.add(world, this.body);
@@ -27,7 +28,7 @@ class Paper
 			//strokeWeight(4);
 			fill(128,128,128);
 			imageMode(CENTER);
-			image(this.image,0,0,this.r);
+			image(this.image,0,0,this.r,this.r);
 			pop()
 			
 	}
